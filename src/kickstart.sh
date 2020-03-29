6fae501186d3ba646f07aa8bfd56297062d5b3d9
robbradyire <robbradyire@gmail.com>
2020-03-29 11:24:18 +0100

    Add to install list

--- src/kickstart.sh
+++ src/kickstart.sh
@@ -3,7 +3,26 @@
 apt-get update
 
 # Install some useful things
+
+## CLI tools
+apt-get install -y bash-completion
 apt-get install -y curl
-apt-get install -y firefox
+apt-get install -y xclip
+
+## Dev
 apt-get install -y git
+apt-get install -y vagrant
 apt-get install -y vim
+apt-get install -y virtualbox
+
+## Browsers
+apt-get install -y firefox
+apt-get install -y google-chrome-stable
+
+## Editing
+apt-get install -y darktable
+apt-get install -y gimp
+
+## Fun times
+apt-get install -y discord
+apt-get install -y steam
